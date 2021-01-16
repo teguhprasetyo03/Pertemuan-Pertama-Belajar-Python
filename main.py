01838d21075a6167cd842ee2b2201c03e5f2d140
Teguh Prasetyo <teguhprasetyo261211@gmail.com>
2021-01-16 20:57:06 +0700

    publish project

---
+++ main.py
@@ -0,0 +1,15 @@
+print("HelLLo world \n")
+print('HELLO KAKAK')
+
+#ini adalah komentar
+# perkalian
+print(4*10)
+
+#pembagian
+print(250/23)
+
+#penjumlahan
+print(1+5+10)
+
+#pengurangan
+print(100.12 - 15.5)
---
+++ variabel.py
@@ -0,0 +1,16 @@
+#variabel
+#variabel adalah tempat untuk menampung sebuah nilai
+
+nama = "Andi Teguh"
+nama = "Teguh Prasetyo"
+First_name = "Andi"
+umur = 24
+umur = 22
+
+
+sukses = False
+
+print(nama)
+print(umur)
+print(sukses)
+print(PI)
